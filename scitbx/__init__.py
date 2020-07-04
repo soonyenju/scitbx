037e13dd7f857c822528ee01b6be6963c2f0d5c2
Songyan Zhu <zhusy93@gmail.com>
2020-07-04 08:59:59 +0100

    Update __init__.py

--- scitbx/__init__.py
+++ scitbx/__init__.py
@@ -1,6 +1,6 @@
 from .manage_yaml import Yaml
 from .manage_time import Montre
 from .manage_direc import create_all_parents, searching_all_files
-import load_google_drive
+from . import load_google_drive
 
 __all__ = ["Yaml", "Montre", "create_all_parents", "searching_all_files", "load_google_drive"]
