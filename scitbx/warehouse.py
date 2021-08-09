d8207ebaed2a9f18a239fc0e567e612942341d6f
Zhu <soonyenju@foxmail.com>
2021-08-09 13:56:53 +0100

    warehouse update

--- scitbx/warehouse.py
+++ scitbx/warehouse.py
@@ -62,3 +62,63 @@ fig = ax.get_figure()
 handles, labels = ax.get_legend_handles_labels()
 by_label = dict(zip(labels, handles))
 ax.legend(by_label.values(), by_label.keys(), loc = "upper center", framealpha = 0.1, frameon = True , bbox_to_anchor=(0.5, 1.1), ncol = 4)
+
+# ============================================================================
+# split one column into three and rename them
+df = df.join(df["date"].str.split("_", expand=True).astype(int).rename(columns = {0: "year", 1: "month", 2: "day"}))
+
+# ============================================================================
+# vertical barplots
+
+# site wise r2
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.set(style="whitegrid")
+
+df = read_df_dn()
+df["site"] = df["site"] + "(" + df["igbp"] + ")"
+
+time_type = "diel"
+
+# print(df.columns)
+
+df["total"] = df[f"MDS_{time_type}_r2"] + df[f"RFR_{time_type}_r2"]
+df["dif_r2"] = df[f"RFR_{time_type}_r2"] - df[f"MDS_{time_type}_r2"]
+# Initialize the matplotlib figure
+f, ax = plt.subplots(figsize=(8, 18))
+
+df = df.sort_values("total", ascending=False)
+# split fig
+df = df.iloc[100::, :]
+print(len(df))
+
+# Plot the total crashes
+sns.set_color_codes("pastel")
+g = sns.barplot(x="total", y="site", data=df,
+            label=f"MDS $r^2$", color="b")
+
+# Plot the crashes where alcohol was involved
+sns.set_color_codes("muted")
+sns.barplot(x=f"RFR_{time_type}_r2", y="site", data=df,
+            label=f"RFR $r^2$", color="b")
+
+# Add a legend and informative axis label
+ax.legend(ncol=2, loc="lower right", frameon=True)
+ax.set(xlim=(0, 2), ylabel="",
+       xlabel=f"$The \; r^2 \; comparison$")
+sns.despine(left=True, bottom=True)
+
+for i in range(len(df)):
+    p = ax.patches[i]
+    height = p.get_height()
+    width = p.get_width()
+    if p.get_x() + width >= 8:
+        x_pos = 8
+    else:
+        x_pos = p.get_x() + width
+    # ax.text(x_pos, p.get_y() + height/2, np.round(df["dif_bias"].values[i], 2))
+    ax.annotate(f"{np.round(df['dif_r2'].values[i], 2)}", (x_pos, p.get_y() + height))
+    
+
+f.savefig(f"3drivers/site_r2_compare_sort_dif_{time_type}_b.pdf", dpi = 600, format = "pdf", bbox_inches = "tight")
