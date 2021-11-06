0d1c2ec1df48315d044a4703fe0fdfc799c8c723
Songyan Zhu <zhusy93@gmail.com>
2021-11-06 15:46:56 +0000

    Update warehouse.py

--- scitbx/warehouse.py
+++ scitbx/warehouse.py
@@ -236,3 +236,130 @@ set.intersection(*map(set,d))
 # solution2:
 # apply intersect1d to (a list of) multiple lists:
 reduce(np.intersect1d, d)
+
+
+# =========================================================================================================================
+'''
+Cirular bar plot
+'''
+# https://www.python-graph-gallery.com/circular-barplot-with-groups
+
+OFFSET = np.pi / 2
+
+def get_label_rotation(angle, offset):
+    # Rotation must be specified in degrees :(
+    rotation = np.rad2deg(angle + offset)
+    if angle <= np.pi:
+        alignment = "right"
+        rotation = rotation + 180
+    else: 
+        alignment = "left"
+    return rotation, alignment
+
+def add_labels(angles, values, labels, offset, ax):
+    
+    # This is the space between the end of the bar and the label
+    padding = 4
+    
+    # Iterate over angles, values, and labels, to add all of them.
+    for angle, value, label, in zip(angles, values, labels):
+        angle = angle
+        
+        # Obtain text rotation and alignment
+        rotation, alignment = get_label_rotation(angle, offset)
+
+        # And finally add the text
+        ax.text(
+            x=angle, 
+            y=value + padding, 
+            s=label, 
+            ha=alignment, 
+            va="center", 
+            rotation=rotation, 
+            rotation_mode="anchor"
+        ) 
+
+
+# Ensures reproducibility of random numbers
+rng = np.random.default_rng(123)
+
+# Build a dataset
+df = pd.DataFrame({
+    "name": [f"item {i}" for i in range(1, 51)],
+    "value": rng.integers(low=30, high=100, size=50),
+    "group": ["A"] * 10 + ["B"] * 20 + ["C"] * 12 + ["D"] * 8
+})
+
+# # Build a dataset
+# df = pd.DataFrame({
+#     "name": [f"item {i}" for i in range(1, 32 * 6 + 1)],
+#     "value": rng.integers(low=30, high=100, size=32 * 6),
+#     "group": ["A"] * 32 + ["B"] * 64 + ["C"] * 32 + ["D"] * 64
+# })
+
+# Show 3 first rows
+df.head(3)
+
+
+# All this part is like the code above
+VALUES = df["value"].values
+LABELS = df["name"].values
+GROUP = df["group"].values
+
+PAD = 3
+ANGLES_N = len(VALUES) + PAD * len(np.unique(GROUP))
+ANGLES = np.linspace(0, 2 * np.pi, num=ANGLES_N, endpoint=False)
+WIDTH = (2 * np.pi) / len(ANGLES)
+
+offset = 0
+IDXS = []
+GROUPS_SIZE = [10, 20, 12, 8]
+# GROUPS_SIZE = [32, 64, 32, 64]
+for size in GROUPS_SIZE:
+    IDXS += list(range(offset + PAD, offset + size + PAD))
+    offset += size + PAD
+
+fig, ax = plt.subplots(figsize=(20, 10), subplot_kw={"projection": "polar"})
+ax.set_theta_offset(OFFSET)
+ax.set_ylim(-100, 100)
+ax.set_frame_on(False)
+ax.xaxis.grid(False)
+ax.yaxis.grid(False)
+ax.set_xticks([])
+ax.set_yticks([])
+
+# GROUPS_SIZE = [10, 20, 12, 8]
+COLORS = [f"C{i}" for i, size in enumerate(GROUPS_SIZE) for _ in range(size)]
+
+ax.bar(
+    ANGLES[IDXS], VALUES, width=WIDTH, color=COLORS, 
+    edgecolor="white", linewidth=2
+)
+
+add_labels(ANGLES[IDXS], VALUES, LABELS, OFFSET, ax)
+
+# Extra customization below here --------------------
+
+# This iterates over the sizes of the groups adding reference
+# lines and annotations.
+
+offset = 0 
+for group, size in zip(["A", "B", "C", "D"], GROUPS_SIZE):
+    # Add line below bars
+    x1 = np.linspace(ANGLES[offset + PAD], ANGLES[offset + size + PAD - 1], num=50)
+    ax.plot(x1, [-5] * 50, color="#333333")
+    
+    # Add text to indicate group
+    ax.text(
+        np.mean(x1), -20, group, color="#333333", fontsize=14, 
+        fontweight="bold", ha="center", va="center"
+    )
+    
+    # Add reference lines at 20, 40, 60, and 80
+    x2 = np.linspace(ANGLES[offset], ANGLES[offset + PAD - 1], num=50)
+    ax.plot(x2, [20] * 50, color="#bebebe", lw=0.8)
+    ax.plot(x2, [40] * 50, color="#bebebe", lw=0.8)
+    ax.plot(x2, [60] * 50, color="#bebebe", lw=0.8)
+    ax.plot(x2, [80] * 50, color="#bebebe", lw=0.8)
+    
+    offset += size + PAD
