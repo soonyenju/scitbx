de01ad3b5a8fa2dd36994f7f6d0b6a651d9c734c
Songyan Zhu <zhusy93@gmail.com>
2022-01-12 15:06:08 +0000

    Update __init__.py

--- scitbx/__init__.py
+++ scitbx/__init__.py
@@ -5,6 +5,7 @@ from . import load_google_drive
 from . import google
 from . import stutils
 from . import meteo
+from . import easy_import
 from .pickle_wrapper import load_pickle, dump_pickle
 from .regress2 import regress2
 
@@ -14,5 +15,6 @@ __all__ = [
     "create_all_parents", "searching_all_files", "unzip", "pbar",
     "load_google_drive", "google",
     "load_pickle", "dump_pickle",
-    "stutils", "regress2"
+    "stutils", "regress2",
+    "easy_import"
     ]
