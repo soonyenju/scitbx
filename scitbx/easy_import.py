9b25d1dad4db2b2ce290cec904dfab2e2844011f
Songyan Zhu <zhusy93@gmail.com>
2022-01-18 09:18:54 +0000

    Update easy_import.py

--- scitbx/easy_import.py
+++ scitbx/easy_import.py
@@ -1,3 +1,4 @@
+from operator import imod
 import os
 import sys
 import warnings
@@ -7,4 +8,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from pathlib import Path
 from copy import deepcopy
-from datetime import datetime
+from scipy import stats
+from datetime import datetime, timedelta
+try:
+    import seaborn as sns
+except Exception as e:
+    print(e)
+try:
+    import xarray as xr
+except Exception as e:
+    print(e)
