9d4d0f367f08ab1448aaa02cc61c5901aac5da79
Songyan Zhu <zhusy93@gmail.com>
2022-09-22 14:55:30 +0100

    Update __init__.py

--- scitbx/__init__.py
+++ scitbx/__init__.py
@@ -4,7 +4,7 @@ from .manage_file import create_all_parents, searching_all_files, unzip, pbar
 from . import load_google_drive
 from . import google
 from . import stutils
-from . import meteo
+from . import meteo as stmeteo
 from . import easy_import
 # from .pickle_wrapper import load_pickle, dump_pickle
 from .regress2 import regress2
@@ -14,7 +14,7 @@ __all__ = [
     "Yaml", 
     "Montre", 
     "create_all_parents", "searching_all_files", "unzip", "pbar",
-    "load_google_drive", "google",
+    "load_google_drive", "google", "stmeteo",
     # "load_pickle", "dump_pickle",
     "stutils", "regress2", 'concordance_correlation_coefficient',
     "easy_import"
