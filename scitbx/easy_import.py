61566bcb000facb8fc3ee933c4c0b76e72cbeb1b
Songyan Zhu <zhusy93@gmail.com>
2023-03-23 09:58:16 +0000

    Update easy_import.py

--- scitbx/easy_import.py
+++ scitbx/easy_import.py
@@ -1,3 +1,4 @@
+import pip
 from operator import imod
 import os
 import sys
@@ -14,9 +15,13 @@ try:
     import seaborn as sns
 except Exception as e:
     print(e)
+    install('seaborn')
+    import seaborn as sns
 try:
     import xarray as xr
 except Exception as e:
     print(e)
+    install('xarray')
+    import xarray as xr
 
 colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
