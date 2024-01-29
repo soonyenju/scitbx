998c29f5c1431e916874d08c0303207edc6d38e0
Songyan Zhu <zhusy93@gmail.com>
2024-01-29 15:24:30 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name = "scitbx",
-	version = "0.0.55",
+	version = "0.0.56",
 	keywords = ("atmos/geo science assitance"),
 	description = "draw and basic calculations/conversions",
 	long_description = "coming soon",
