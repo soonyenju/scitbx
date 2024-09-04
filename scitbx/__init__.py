bfebb35fae1604621e1dd12004e1c3dc269bfbe9
Songyan Zhu <zhusy93@gmail.com>
2024-09-04 10:16:13 +0100

    Update __init__.py

--- scitbx/__init__.py
+++ scitbx/__init__.py
@@ -1,5 +1,3 @@
-import numpy as np
-import pandas as pd
 from . import easy_import
 from . import google
 from . import stutils
@@ -16,6 +14,4 @@ __all__ = [
     "stutils",
     "sciplt",
     "bigleaf",
-    "np",
-    "pd"
     ]
