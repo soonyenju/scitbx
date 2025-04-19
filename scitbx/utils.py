e3a7d7d7eda2bca4a2a54406ebafa17a801f274a
Songyan Zhu <zhusy93@gmail.com>
2025-04-19 09:30:09 +0100

    Update utils.py

--- scitbx/utils.py
+++ scitbx/utils.py
@@ -61,88 +61,6 @@ def timeit(cur_iter, whole_counts, begin_time, ratio = 10, time_unit = 'seconds'
         print(f'{progress} %, {time_dif} {time_unit_map[time_unit]}', end = '|')
     if cur_iter >= whole_counts:
         print('', end = '\n')
-
-def stats_summary(df):
-    min_ = df.min().to_frame().T
-    Q1 = df.quantile(0.25).to_frame().T
-    median_ = df.quantile(0.5).to_frame().T
-    mean_ = df.mean().to_frame().T
-    Q3 = df.quantile(0.75).to_frame().T
-    max_ = df.max().to_frame().T
-    df_stats = pd.concat([min_, Q1, median_, mean_, Q3, max_])
-    df_stats.index = ["Min", "Q1", "Median", "Mean", "Q3", "Max"]
-    return df_stats
-
-def stats_measures(x, y, return_dict = False):
-    slope, intercept, rvalue, pvalue, stderr = stats.linregress(x, y)
-    mse = mean_squared_error(x, y)
-    r2 = rvalue ** 2
-    rmse = np.sqrt(mse)
-    mbe = (y - x).mean()
-    if return_dict:
-        return {
-            "R2": r2,
-            "SLOPE": slope,
-            "RMSE": rmse,
-            "MBE": mbe
-        }
-    else:
-        return [r2, slope, rmse, mbe]
-
-def stats_measures_full(x, y):
-    from sklearn.metrics import explained_variance_score, max_error, mean_absolute_error, mean_squared_error, mean_squared_log_error, median_absolute_error, r2_score, mean_poisson_deviance, mean_gamma_deviance, mean_tweedie_deviance
-    # from sklearn.metrics import mean_absolute_percentage_error
-    slope, intercept, rvalue, pvalue, stderr = stats.linregress(x, y)
-    mse = mean_squared_error(x, y)
-    r2 = rvalue ** 2
-    rmse = np.sqrt(mse)
-    mbe = (y - x).mean()
-    # ----------------------------------------------------------------
-    pearsonr = stats.pearsonr(x, y)
-    evs = explained_variance_score(x, y)
-    me = max_error(x, y)
-    mae = mean_absolute_error(x, y)
-    msle = mean_squared_log_error(x, y)
-    meae = median_absolute_error(x, y)
-    r2_score = r2_score(x, y)
-    mpd = mean_poisson_deviance(x, y)
-    mgd = mean_gamma_deviance(x, y)
-    mtd = mean_tweedie_deviance(x, y)
-    return {
-        "R2": r2,
-        "SLOPE": slope,
-        "RMSE": rmse,
-        "MBE": mbe,
-        "INTERCEPT": intercept,
-        "PVALUE": pvalue,
-        "STDERR": stderr,
-        "PEARSON": pearsonr,
-        "EXPLAINED_VARIANCE": evs,
-        "MAXERR": me,
-        "MAE": mae,
-        "MSLE": msle,
-        "MEDIAN_AE": meae,
-        "R2_SCORE": r2_score,
-        "MPD": mpd,
-        "MGD": mgd,
-        "MTD": mtd
-    }
-
-def stats_measures_df(df, name1, name2, return_dict = False):
-    slope, intercept, rvalue, pvalue, stderr = stats.linregress(df[name1], df[name2])
-    mse = mean_squared_error(df[name1], df[name2])
-    r2 = rvalue ** 2
-    rmse = np.sqrt(mse)
-    mbe = (df[name2] - df[name1]).mean()
-    if return_dict:
-        return {
-            "R2": r2,
-            "SLOPE": slope,
-            "RMSE": rmse,
-            "MBE": mbe
-        }
-    else:
-        return [r2, slope, rmse, mbe]
     
 def dateparse(dt, format = '%Y-%m-%d'):
     return pd.to_datetime(dt, format = format)
@@ -216,15 +134,6 @@ def timedif(dt1, dt2, mode = 'None'):
         return dif.total_seconds() / 60 / 60 / 24 / 365
     else:
         return dif
-
-def create_folder(des):
-    # exist_ok = False: DO NOT make if the directory exists!
-    try:
-        des.mkdir(mode = 0o777, parents = True, exist_ok = False)
-        print(f'Directory made: {des} ')
-    except FileExistsError as e:
-        print('Not creating, target directory exists!')
-        # FileExistsError
 
 def format_axis_datetime(ax, fmt = '%m/%Y', which = 'x'):
     import matplotlib.dates as mdates
@@ -585,43 +494,6 @@ class ByteSize(int):
 def quiet():
     import warnings
     warnings.simplefilter('ignore')
-
-
-def get_r2(x, y):
-    try:
-        x_bar = x.mean()
-    except:
-        x_bar = np.mean(x)
-
-    r2 = 1 - np.sum((x - y)**2) / np.sum((x - x_bar)**2)
-    return r2
-
-def get_rmse(observations, estimates):
-    return np.sqrt(((estimates - observations) ** 2).mean())
-
-def calculate_R2(y_true, y_pred):
-    """
-    Calculate the R^2 (coefficient of determination).
-
-    Args:
-        y_true (array-like): Actual values of the dependent variable.
-        y_pred (array-like): Predicted values of the dependent variable.
-
-    Returns:
-        float: The R^2 value.
-    """
-    y_true = np.array(y_true)
-    y_pred = np.array(y_pred)
-
-    # Residual sum of squares
-    ss_res = np.sum((y_true - y_pred) ** 2)
-
-    # Total sum of squares
-    ss_tot = np.sum((y_true - np.mean(y_true)) ** 2)
-
-    # R^2 calculation
-    R2 = 1 - (ss_res / ss_tot)
-    return R2
 
 def get_github_file(url, target_directory):
     import requests, zipfile, io
