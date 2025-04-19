8c07d0e4351f702c7830eed74e6c07eb7446e47b
Songyan Zhu <zhusy93@gmail.com>
2025-04-19 09:06:36 +0100

    Update __init__.py

--- scitbx/__init__.py
+++ scitbx/__init__.py
@@ -2,7 +2,6 @@ from . import easy_import
 from . import google
 from . import utils
 from . import sciplt
-# from . import bigleaf
 from . import commons
 
 __all__ = ["easy_import", "google", "utils", "sciplt", "commons"]
