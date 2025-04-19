5283cecb4102ba31831a765b9a5fbb9f2a660abd
Songyan Zhu <zhusy93@gmail.com>
2025-04-19 07:44:19 +0100

    Update easy_import.py

--- scitbx/easy_import.py
+++ scitbx/easy_import.py
@@ -68,5 +68,3 @@ install('scigeo')
 install('scieco')
 install('sciml')
 from sciml import pipelines
-
-# colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
