03dfa70f1237c42616fbfe558e7b2b8ce2512554
Songyan Zhu <zhusy93@gmail.com>
2025-07-08 20:53:55 +0100

    Update easy_import.py

--- scitbx/easy_import.py
+++ scitbx/easy_import.py
@@ -18,6 +18,7 @@ from pathlib import Path
 
 # Third-party imports
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 import numpy as np
 import pandas as pd
 from scipy import stats
